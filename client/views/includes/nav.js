09a052e989d1baed08f01a93ba760cb5436e2251
Marcus Russi <marcusrussi@gmail.com>
2013-10-24 20:42:54 -0400

    Nav affix, content, background

--- client/views/includes/nav.js
+++ client/views/includes/nav.js
@@ -1,5 +1,17 @@
 Template.nav.helpers({
 	fixed: function () {
-		return Session.get('fixed');
+		return false; //Session.get('fixed');
 	}
 });
+
+Template.nav.created = function () {
+	Meteor.defer(function () {
+		$('nav').affix({
+			offset: {
+				top: function () {
+					return $('.welcome').height();
+				}
+			}
+		});	
+	});
+}
